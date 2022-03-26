65fd7ef3cc59298130bff04e949131d66c05e4bd
yyogesh <yogeshggn1@gmail.com>
2022-03-26 18:27:36 +0530

    assertion

--- cypress/integration/Test/basic.spec.js
+++ cypress/integration/Test/basic.spec.js
@@ -1,5 +1,10 @@
+/// <reference types="Cypress" />
 describe("Basic of cypress", () => {
     it("First cypress test", () => {
-        cy.visit('https://www.amazon.in/');
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.products .product').should('have.length', 4);
+        cy.get('.product:visible').should('have.length', 4);
     })
 })
