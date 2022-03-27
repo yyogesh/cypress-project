d03dcca0010524dfe8c304c3c4ef1839f95d6a3a
yyogesh <yogeshggn1@gmail.com>
2022-03-27 14:34:24 +0530

    cypress click, find

--- cypress/integration/Test/basic.spec.js
+++ cypress/integration/Test/basic.spec.js
@@ -6,5 +6,16 @@ describe("Basic of cypress", () => {
         cy.wait(2000);
         cy.get('.products .product').should('have.length', 4);
         cy.get('.product:visible').should('have.length', 4);
+        cy.get('.products').find('.product').should('have.length', 4);
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text();
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click();
+                // $el is promise it need to resolve so wrap it
+                //Yield the object passed into .wrap(). If the object is a promise, yield its resolved value.
+            }
+        });
     })
 })
