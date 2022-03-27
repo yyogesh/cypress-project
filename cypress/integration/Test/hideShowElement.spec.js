78997f9472d2fd64b9b085f13fe730d9f55ad38d
yyogesh <yogeshggn1@gmail.com>
2022-03-27 16:30:53 +0530

    hide show test case

---
+++ cypress/integration/Test/hideShowElement.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+describe("Basic of cypress", () => {
+    it("Autocomplete cypress test", () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('#displayed-text').should('be.visible');
+        cy.get('#hide-textbox').click();
+        cy.get('#displayed-text').should('not.be.visible');
+        cy.get('#show-textbox').click();
+        cy.get('#displayed-text').should('be.visible');
+    })
+});
---
+++ cypress/integration/Test/radio.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+describe("Basic of cypress", () => {
+    it("Autocomplete cypress test", () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+    })
+});
